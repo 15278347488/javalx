8fb8bfd08be6da8846d58744bd66c78626fe85c5
奇怪的猪 <74753156+15278347488@users.noreply.github.com>
2020-11-30 15:31:54 +0800

    Update 113001.继承子类.App.java

--- 113001.继承子类.App.java
+++ 113001.继承子类.App.java
@@ -5,6 +5,10 @@ public class App extends R{
 	public static void main(String[] args) {
 		System.out.println(boy_filePath);
 		System.out.println(girl_filePath);
-		System.out.println(array2d[0][1]);
+		for (int row = 0; row < array2d.length; row++) {
+			for (int column = 0; column < array2d[0].length; column++) {
+				System.out.println(array2d[row][column]);
+			}
+		}
 	}
 }
