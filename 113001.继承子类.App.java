0e67291be5e1614521a9bd1e1b328b490f80bc4a
奇怪的猪 <74753156+15278347488@users.noreply.github.com>
2020-11-30 15:08:46 +0800

    Create 113001.继承子类.App.java

---
+++ 113001.继承子类.App.java
@@ -0,0 +1,10 @@
+package com.a;
+
+
+public class App extends R{
+	public static void main(String[] args) {
+		System.out.println(boy_filePath);
+		System.out.println(girl_filePath);
+		System.out.println(array2d[0][1]);
+	}
+}
